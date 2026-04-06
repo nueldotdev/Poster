4d01c4e822e0e8db3798b1cbb6e71494c2e08ac4
nuel <walternuel02@gmail.com>
2026-04-06 04:10:10 +0100

    fix: setup errors fixed

--- forge.config.ts
+++ forge.config.ts
@@ -6,10 +6,13 @@ import { MakerRpm } from "@electron-forge/maker-rpm";
 import { VitePlugin } from "@electron-forge/plugin-vite";
 import { FusesPlugin } from "@electron-forge/plugin-fuses";
 import { FuseV1Options, FuseVersion } from "@electron/fuses";
+import { AutoUnpackNativesPlugin } from "@electron-forge/plugin-auto-unpack-natives";
 
 const config: ForgeConfig = {
   packagerConfig: {
-    asar: true,
+    asar: {
+      unpack: "**/node_modules/{sharp,wallpaper}/**/*"
+    },
   },
   rebuildConfig: {},
   makers: [
@@ -24,6 +27,7 @@ const config: ForgeConfig = {
     new MakerDeb({}),
   ],
   plugins: [
+    new AutoUnpackNativesPlugin({}),
     new VitePlugin({
       // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
       // If you are familiar with Vite configuration, it will look really familiar.
